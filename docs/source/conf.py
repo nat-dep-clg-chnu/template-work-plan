011702bf00dec2dd0b063ce2f66466d245e44c39
Природниче відділення ВСП "Фаховий Коледж ЧНУ <102409039+nat-dep-clg-chnu@users.noreply.github.com>
2022-11-16 12:49:57 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Природниче Відділення'
 author = 'Природниче відділення'
 
 # The full version, including alpha/beta/rc tags
-release = '0'
+# release = '0'
 
 
 # -- General configuration ---------------------------------------------------
